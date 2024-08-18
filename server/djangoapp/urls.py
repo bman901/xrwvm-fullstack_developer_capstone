c0a7d99692565b7eb9f5d61db488d29845cbb7e7
Ben Kehoe <77235105+bman901@users.noreply.github.com>
2024-08-18 12:06:19 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,18 +15,30 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for get cars
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 
     # path for get dealers
-    path(route='get_dealers', view=views.get_dealerships, name ='getdealers'),
+    path(route='get_dealers', view=views.get_dealerships, name='getdealers'),
 
     # path for get dealers by state
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
 
     # path for dealer reviews view
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='get_dealer_reviews'),
+    path(
+        route='dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='get_dealer_reviews'
+    ),
 
     # path for add a review view
-    path(route='postreview/<int:dealer_id>', view=views.add_review, name='addreview'),
+    path(
+        route='postreview/<int:dealer_id>',
+        view=views.add_review,
+        name='addreview'
+    ),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
