f006122bf9472a7b6cc107ecdb964fa1dc7ee169
Ben Kehoe <77235105+bman901@users.noreply.github.com>
2024-08-18 12:24:43 +0100

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,8 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'https://benkehoe90-8000.theiadockernext'
                  '-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
-CSRF_TRUSTED_ORIGINS = ['https://benkehoe90-8000.theiadockernext'
-                        '-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://benkehoe90-8000.theiadockernext-1-labs'
+                        '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
